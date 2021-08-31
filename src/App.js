7d742ad9536170315513ed31e5e9cff0ca9f4997
chimdie <nzechimdi@yahoo.com>
2021-08-31 12:27:27 +0100

    fix/restructured the file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import NavBar from "./components/NavBar";
-import Main from './components/Main'
-
-import GlobalStyle from "./theme/globalStyle";
+import Main from "./components/Main";
 import styled from "styled-components";
 import bg from "./images/bg.png";
 
@@ -11,7 +9,7 @@ const Layout = styled.div`
   background-position: center center;
   min-height: 100vh;
 
-  @media only screen and (max-width: 768px){
+  @media only screen and (max-width: 768px) {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -21,13 +19,10 @@ const Layout = styled.div`
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Layout>
-        <NavBar />
-        <Main/>
-      </Layout>
-    </>
+    <Layout>
+      <NavBar />
+      <Main />
+    </Layout>
   );
 }
 
